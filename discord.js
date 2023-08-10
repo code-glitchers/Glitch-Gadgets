1a072eec1c268e73d1167320530374a670ddaa73
∅ח א כ ד ה יד∅ <cubedimension@protonmail.com>
2023-08-10 15:54:18 +0530

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     const clientId = "1001933562565644338"; // Replace with your actual client ID
-    const redirectUri = "https://code-glitchers.github.io/Glitch-Gadgets/"; // Your actual redirect URI
+    const redirectUri = "https://code-glitchers.github.io/Glitch-Gadgets/callback"; // Your actual redirect URI
     const scope = "identify";
 
     const loginButton = $("#loginButton");
@@ -9,7 +9,7 @@ $(document).ready(function() {
     const usernameElement = $("#username");
 
     loginButton.click(function() {
-        window.location.href = https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=${scope};
+        window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=${scope}`;
     });
 
     function parseHashParams() {
@@ -28,14 +28,14 @@ $(document).ready(function() {
 
         fetch("https://discord.com/api/users/@me", {
             headers: {
-                Authorization: Bearer ${params.access_token},
+                Authorization: `Bearer ${params.access_token}`,
             },
         })
         .then(response => response.json())
         .then(user => {
-            const avatarUrl = user.avatar ? https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png : "https://discord.com/assets/322c936a8c8be1b803cd94861bdfa868.png";
+            const avatarUrl = user.avatar ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png` : "https://discord.com/assets/322c936a8c8be1b803cd94861bdfa868.png";
             avatarElement.attr("src", avatarUrl);
-            usernameElement.text(Hello, ${user.username}#${user.discriminator}!);
+            usernameElement.text(`Hello, ${user.username}#${user.discriminator}!`);
             userInfoDiv.show();
             loginButton.hide();
         })
